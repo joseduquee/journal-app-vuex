50a37398889ffbd30e58e7862e35162d465c2a03
Alexander Duque <joseduquee@gmail.com>
2022-04-21 14:14:29 +0200

    first commit journalapp vue

---
+++ src/modules/daybook/layouts/DayBookLayout.vue
@@ -0,0 +1,27 @@
+<template>
+  <NavbarApp />
+  <div class="d-flex">
+      <div class="col-4">
+          <EntryList />
+      </div>
+      <div>
+          <router-view />
+      </div>
+  </div>
+</template>
+
+<script>
+
+import { defineAsyncComponent } from 'vue'
+
+export default {
+      components: {
+          NavbarApp: defineAsyncComponent(() => import('../components/NavbarApp.vue')),
+          EntryList: defineAsyncComponent(() => import('../components/EntryList.vue'))
+      }          
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/modules/daybook/store/journal/state.js
@@ -0,0 +1,25 @@
+
+
+export default () => ({
+    isLoading: true,
+    entries: [
+        {
+            id: new Date().getTime(),
+            date: new Date().toDateString(),
+            text: 'Loremo wefwer wefwrgppw commodo',
+            picture: null
+        },
+        {
+            id: new Date().getTime() + 1000,
+            date: new Date().toDateString(),
+            text: 'Quiubo pues quioes ieubuer erighiwepo',
+            picture: null
+        },
+        {
+            id: new Date().getTime() + 2000,
+            date: new Date().toDateString(),
+            text: 'en esa rjkne 0wergf ohowefwe',
+            picture: null
+        },
+    ]
+})
