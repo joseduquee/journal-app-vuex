9b35c25b59d9ce3b5a9a846030b334cebebebe3c
Alexander Duque <joseduquee@gmail.com>
2022-04-22 23:55:41 +0200

    cargar datos desde state

--- src/modules/daybook/store/journal/mutations.js
+++ src/modules/daybook/store/journal/mutations.js
@@ -1,7 +1,8 @@
 
 
-export const setEntries = ( /*state*/ ) => {
-
+export const setEntries = ( state, entries ) => {
+    state.entries = [ ...state.entries, ...entries ]
+    state.isLoading = false
 }
 
 export const updateEntry = ( /*state*/ ) => {
--- src/modules/daybook/store/journal/state.js
+++ src/modules/daybook/store/journal/state.js
@@ -2,24 +2,5 @@
 
 export default () => ({
     isLoading: true,
-    entries: [
-        {
-            id: '1',
-            date: new Date().toDateString(),
-            text: 'Loremo wefwer wefwrgppw commodo Loremo wefwer wefwrgppw commodo Loremo wefwer wefwrgppw commodo Loremo wefwer wefwrgppw commodo Loremo wefwer wefwrgppw commodo Loremo wefwer wefwrgppw commodo Loremo wefwer wefwrgppw commodo',
-            picture: null
-        },
-        {
-            id: '2',
-            date: new Date().toDateString(),
-            text: 'Quiubo pues quioes ieubuer erighiwepo',
-            picture: null
-        },
-        {
-            id: '3',
-            date: new Date().toDateString(),
-            text: 'en esa rjkne 0wergf ohowefwe',
-            picture: null
-        },
-    ]
+    entries: []
 })
